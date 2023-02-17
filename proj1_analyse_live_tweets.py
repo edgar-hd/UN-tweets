714e95838305425231762323f38120cbc5cbb6f5
edgar-hd <edgar.hd@protonmail.com>
2023-02-17 21:42:36 +0100

    uncomment bit

--- proj1_analyse_live_tweets.py
+++ proj1_analyse_live_tweets.py
@@ -152,12 +152,12 @@ stop = set(stopwords.words('english') + list(string.punctuation))
 stop.update(['’','https','un','amp','``',"''","'s",'..','...',"n't",'--','”','–','//','“','like','also','put','ask','w/','unitednations'])
 print("Defined stopwords")
 
-# def clean_sentence(sentence):
-#     cleaned = [i for i in word_tokenize(sentence.lower()) if i not in stop]
-#     return cleaned
+def clean_sentence(sentence):
+    cleaned = [i for i in word_tokenize(sentence.lower()) if i not in stop]
+    return cleaned
 
-# words_List = [clean_sentence(text) for text in full_set['Text']]
-# print("Performed word cleaning")
+words_List = [clean_sentence(text) for text in full_set['Text']]
+print("Performed word cleaning")
 
 # def get_flat_window(window_index, condition):
 #     all_words = [words_List[i] for i in np.where(window_index & condition)[0]]
